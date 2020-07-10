a2f94ab83228564ea047c9cff506e054bc4f626d
Sughosh Kulkarni <47445832+SughoshKulkarni@users.noreply.github.com>
2020-07-09 20:12:05 -0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -163,7 +163,7 @@ function initAudio() {
     if (!navigator.requestAnimationFrame)
         navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
 
-    navigator.getUserMedia(
+    navigator.mediaDevices.getUserMedia(
         {
             "audio": {
                 "mandatory": {
